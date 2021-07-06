af0a90b31af1da14be7bb258c7308d73cb08d25b
Rao Hassan <raohassan4212@gmail.com>
2021-07-06 15:45:08 +0500

    covid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux'
+import Store from './store';
+import Navbar from './components/Navbar';
+import InfoPanel from './components/InfoPanel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Provider store={Store}>
+        <Navbar />
+        <InfoPanel />
+      </Provider>
     </div>
   );
 }
